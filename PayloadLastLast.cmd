99d3bac8f694789c16a761646d8991f9f65f4a2a
SlonNaVipase <alexshirinkin24@gmail.com>
2025-05-29 15:26:12 +0500

    Update PayloadLastLast.cmd

--- PayloadLastLast.cmd
+++ PayloadLastLast.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 chcp 65001
-echo "exploit" > C:\Users\Publicexploit
+echo "exploit" > C:\Users\Public\exploit
 for /l %%q in (0) do (echo "ВЫ_ВЗЛОМАНЫ_НЕМЕДЛЕННО_СООБЩИТЕ_СИСТЕМНОМУ_АДМИНИСТРАТОРУ!!!!!!" 
 timeout 3)
 
